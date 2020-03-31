0aedc7647ab48221464913cc209aa0b0aa7fc94b
garesnap <62925734+garesnap@users.noreply.github.com>
2020-03-31 03:07:31 -0400

    Create palindrome.js

---
+++ palindrome.js
@@ -0,0 +1,20 @@
+function palindrome(str) {
+    let pureString = str.replace(/\W/g, '')
+    pureString = pureString.toLowerCase();
+    pureString = pureString.replace(/[^0-9a-z]/gi, '');
+    let regArray = pureString.split('');
+    if(regArray.join() === regArray.reverse().join()){
+        return true;
+    }
+    else{
+        return false;
+    }
+    
+    
+  }
+  
+  
+  
+console.log(palindrome('_eye'));
+  //console.log(palindrome("hey buddy"));
+

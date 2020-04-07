08c47c096132a8c7f0cd3e6a47a974215bfd1236
Garrett Hensley <62925734+garesnap@users.noreply.github.com>
2020-04-07 18:49:58 -0400

    Organized palindrome.js into a website

--- palindrome.js
+++ palindrome.js
@@ -1,3 +1,9 @@
+const button = document.querySelector('#submit');
+const displayAnswer = document.querySelector('#answer');
+let answerText = '';
+let answer = '';
+
+
 function palindrome(str) {
     let pureString = str.replace(/\W/g, '')
     pureString = pureString.toLowerCase();
@@ -10,7 +16,24 @@ function palindrome(str) {
         return false;
     }
   }
-// testing functionality
-console.log(palindrome('_eye'));
 
+button.addEventListener('click', () => {
+  const textInput = document.querySelector('#textInput');
+  console.log(textInput.value);
+  answer = textInput.value;
+  if(!answer){
+    answerText = '';
+  }
+  else if(palindrome(answer)){
+    answerText = `'${answer}' is a palindrome.`;
+  }
+  else{
+    answerText = `'${answer}' is not a palindrome.`;
+  }
+  displayAnswer.textContent = answerText;
+});
+
+  
+console.log(palindrome('_eye'));
+  //console.log(palindrome("hey buddy"));
 

1e51a3ff2a7efcefd3e1a2f648189b79e2fc5a86
Garrett Hensley <62925734+garesnap@users.noreply.github.com>
2020-04-07 21:15:55 -0400

    Output string's first character now uppercase

--- palindrome.js
+++ palindrome.js
@@ -1,39 +1,40 @@
 const button = document.querySelector('#submit');
 const displayAnswer = document.querySelector('#answer');
+const input = document.getElementById('textInput');
 let answerText = '';
 let answer = '';
 
-
 function palindrome(str) {
-    let pureString = str.replace(/\W/g, '')
-    pureString = pureString.toLowerCase();
-    pureString = pureString.replace(/[^0-9a-z]/gi, '');
-    let regArray = pureString.split('');
-    if(regArray.join() === regArray.reverse().join()){
-        return true;
-    }
-    else{
-        return false;
-    }
+  let pureString = str.replace(/\W/g, '');
+  pureString = pureString.toLowerCase();
+  pureString = pureString.replace(/[^0-9a-z]/gi, '');
+  let regArray = pureString.split('');
+  if (regArray.join() === regArray.reverse().join()) {
+    return true;
+  } else {
+    return false;
   }
+}
+
+input.addEventListener('keyup', (event) => {
+  if (event.keyCode === 13) {
+    button.click();
+  }
+});
 
 button.addEventListener('click', () => {
   const textInput = document.querySelector('#textInput');
-  console.log(textInput.value);
   answer = textInput.value;
-  if(!answer){
+  if (!answer) {
     answerText = '';
-  }
-  else if(palindrome(answer)){
-    answerText = `'${answer}' is a palindrome.`;
-  }
-  else{
-    answerText = `'${answer}' is not a palindrome.`;
+  } else if (palindrome(answer)) {
+    answerText = `'${
+      answer.charAt(0).toUpperCase() + answer.slice(1)
+    }' is a palindrome.`;
+  } else {
+    answerText = `'${
+      answer.charAt(0).toUpperCase() + answer.slice(1)
+    }' is not a palindrome.`;
   }
   displayAnswer.textContent = answerText;
 });
-
-  
-console.log(palindrome('_eye'));
-  //console.log(palindrome("hey buddy"));
-
